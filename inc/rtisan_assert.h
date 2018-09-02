eaa5eac63fb0cd2d2f18656996442cd8ad13637d
Michael Lyle <mlyle@lyle.org>
2018-09-02 14:11:25 -0700

    slight refinements to assert

--- inc/rtisan_assert.h
+++ inc/rtisan_assert.h
@@ -3,6 +3,13 @@
 
 #define assert(x) ((x) ? (void)0 : __assert_impl(__FILE__, __LINE__, #x))
 
+#define abort rtisan_abort
+
+static inline void abort(void)
+{
+	while (1);
+}
+
 static inline void __assert_impl(const char *fn, const int ln,
 		const char *expr)
 {
@@ -14,11 +21,6 @@ static inline void __assert_impl(const char *fn, const int ln,
 #endif
 
 	abort();
-}
-
-inline void abort(void)
-{
-	while (1);
 }
 
 #endif /* __RTISAN_ASSERT_H */
