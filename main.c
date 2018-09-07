71d8c57eda52e5db9a3300b257ff2415cd2aad9a
Michael Lyle <mlyle@lyle.org>
2018-09-07 09:44:49 -0700

    main: add initialization code for SPIs

--- main.c
+++ main.c
@@ -12,7 +12,49 @@
 #include <systick_handler.h>
 #include <rtisan_stream.h>
 
-const DIOInitTag_t leds[8] = {
+#ifndef __linux__
+#include <spi_stm32f3.h>
+
+/* XXX dac clear, dac load, nail to values ... */
+
+static const struct RTSPIF3Pins_s SPI1Pins = {
+	.miso = GPIOA_DIO(6)
+		| INIT_DIO_ALTFUNC_IN(DIO_PULL_UP, 5),
+	.mosi = GPIOA_DIO(7)
+		| INIT_DIO_ALTFUNC_OUT(DIO_PULL_UP, DIO_DRIVE_MEDIUM, false, 5),
+	.sck = GPIOA_DIO(5)
+		| INIT_DIO_ALTFUNC_OUT(DIO_PULL_UP, DIO_DRIVE_MEDIUM, false, 5),
+	.slaves = {
+		GPIOA_DIO(4)
+			| INIT_DIO_OUTPUT(DIO_DRIVE_MEDIUM, false, false),
+		DIO_NULL
+	},
+};
+
+static const struct RTSPIF3Pins_s SPI2Pins = {
+	.miso = GPIOB_DIO(14)
+		| INIT_DIO_ALTFUNC_IN(DIO_PULL_UP, 5),
+	.mosi = DIO_NULL,
+	.sck = GPIOB_DIO(13)
+		| INIT_DIO_ALTFUNC_OUT(DIO_PULL_UP, DIO_DRIVE_MEDIUM, false, 5),
+	.slaves = {
+		DIO_NULL
+	},
+};
+
+static const struct RTSPIF3Pins_s SPI3Pins = {
+	.miso = GPIOB_DIO(4)
+		| INIT_DIO_ALTFUNC_IN(DIO_PULL_UP, 6),
+	.mosi = DIO_NULL,
+	.sck = GPIOB_DIO(3)
+		| INIT_DIO_ALTFUNC_OUT(DIO_PULL_UP, DIO_DRIVE_MEDIUM, false, 6),
+	.slaves = {
+		DIO_NULL
+	},
+};
+#endif
+
+static const DIOInitTag_t leds[8] = {
 #ifndef __linux__
 	GPIOE_DIO(8) | INIT_DIO_OUTPUT(DIO_DRIVE_MEDIUM, false, false),
 	GPIOE_DIO(9) | INIT_DIO_OUTPUT(DIO_DRIVE_MEDIUM, false, false),
@@ -43,6 +85,9 @@ static void USB_LP_IRQHandler(void)
 
 const void *interrupt_vectors[] __attribute((section(".interrupt_vectors"))) =
 {
+	[SPI1_IRQn] = RTSPIF3IRQSPI1,
+	[SPI2_IRQn] = RTSPIF3IRQSPI2,
+	[SPI3_IRQn] = RTSPIF3IRQSPI3,
 	[USB_LP_CAN_RX0_IRQn] = USB_LP_IRQHandler,
 };
 
@@ -91,6 +136,15 @@ int main(void)
 	cdcStream = RTStreamCreate(1, 129, 129, true);
 
 #ifndef __linux__
+	RTSPIPeriph_t SPI1Periph = RTSPIF3Create(1, &SPI1Pins);
+	assert(SPI1Periph);
+
+	RTSPIPeriph_t SPI2Periph = RTSPIF3Create(2, &SPI2Pins);
+	assert(SPI2Periph);
+
+	RTSPIPeriph_t SPI3Periph = RTSPIF3Create(3, &SPI3Pins);
+	assert(SPI3Periph);
+
 	/* XXX begin USB chunk 2 */
 	DIOInit(GPIOA_DIO(11) |
 			INIT_DIO_ALTFUNC_OUT(DIO_PULL_NONE, DIO_DRIVE_STRONG, false, 14));
